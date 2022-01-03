4827bed7194e828244a0f164b19f05c7776c8df7
CharlyBrown99 <mr.charlybrown99@gmail.com>
2022-01-03 23:28:33 +0900

    Updated Monsta-task

--- Monsta-task/src/Header/header.js
+++ Monsta-task/src/Header/header.js
@@ -1,23 +1,15 @@
 import React from 'react';
 import { Container, Navbar, Button } from "react-bootstrap";
-import { AiOutlineMenu } from "react-icons/ai";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../style/style.css';
+
 
 function Header() {
     return (
         <Navbar expand="xxl" className='white'>
             <Container className="justify-content-end">
-                <Navbar className='nav-item items nav-link text-white'>Home</Navbar>
-                <Navbar className='nav-item items nav-link text-white'>Page1</Navbar>
-                <Navbar className='nav-item items nav-link text-white'>Page2</Navbar>
-
-                <Navbar className='nav-item nav-link text-white Nav_icon_button'>
-                    <Button style={{ backgroundColor: 'transparent', border: 0 }}><AiOutlineMenu className='Nav_icon' /></Button>
-                </Navbar>
-
                 <Navbar className='nav-item nav-link text-white'>
-                    <Button className='Nab_button' variant="primary" size="lg">Action Button</Button>
+                    <Button className='Nab_button' variant="primary" size="lg">Connect Wallet</Button>
                 </Navbar>
 
             </Container>
