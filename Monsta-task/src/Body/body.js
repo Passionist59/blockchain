cbf68bc9e338bb9610cdea33626099eb99fa6ce6
CharlyBrown99 <mr.charlybrown99@gmail.com>
2022-01-07 00:16:58 +0900

    Updated Mosta-task

--- Monsta-task/src/Body/body.js
+++ Monsta-task/src/Body/body.js
@@ -1,26 +1,101 @@
 import React, { useState } from 'react';
-import { Container, Button } from "react-bootstrap";
+import { Container, Navbar, Button } from "react-bootstrap";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../style/style.css';
 
 
+const ABI = [
+    {
+        "inputs": [
+            {
+                "internalType": "uint256",
+                "name": "amountIn",
+                "type": "uint256"
+            },
+            {
+                "internalType": "uint256",
+                "name": "amountOutMin",
+                "type": "uint256"
+            },
+            {
+                "internalType": "uint256",
+                "name": "deadline",
+                "type": "uint256"
+            }
+        ],
+        "name": "swapExactMoniForStt",
+        "outputs": [
+            {
+                "internalType": "uint256",
+                "name": "amountOut",
+                "type": "uint256"
+            }
+        ],
+        "stateMutability": "nonpayable",
+        "type": "function"
+    }
+];
+
+const contractAddress = "0x2a3167fc2Fc1130AAd87f13Dec763620E99E18b4";
+
 function Mainbody() {
+    
+    const [btntext, setBtnText] = useState("CONNECT WALLET");
+    const [chainId, setChainId] = useState("");
+
+    // account? btntext = "DISCONNECT" : btntext = "CONNECT WALLET";
+
+    const connectWallet = () => {
+        if (btntext === "DISCONNECT") {
+            alert("CONNECTION is running");
+        } else {
+            connection();
+        }
+    }
+
+
+    const connection = () => {
+        window.monsta
+            .request({ method: 'eth_requestAccounts' })
+            .then(() => {
+                setBtnText("DISCONNECT WALLET");
+                setChainId("0x3e9");
+                window.monsta.on('chainChanged', (_chainId) => window.location.reload());
+
+            })
+            .catch((err) => {
+                if (err.code === 4001) {
+                    console.log('Please connect to Monsta.');
+                } else {
+                    console.error(err);
+                }
+            });
+    }
+
     const [salary, setSalary] = useState("");
     function handlechange(event) {
         setSalary(event.target.value);
     }
 
     const Swap = () => {
-        
+        // const contract = window.monsta
     }
 
     return (
+        
         <div>
+            <Navbar expand="xxl" className='white'>
+                <Container className="justify-content-end">
+                    <Navbar className='nav-item nav-link text-white'>
+                        <Button className='Nab_button' variant="primary" size="lg" onClick={connectWallet} >{btntext}</Button>
+                    </Navbar>
+
+                </Container>
+            </Navbar>
             <Container>
                 <div className='Omni_chain_container'>
                     
                     <div className='calculation_container'>
-
                         <div className='cal_change'>
                             <div className='change_container1'>
 
--- Monsta-task/src/content.js
+++ Monsta-task/src/content.js
@@ -1,11 +1,9 @@
 import React from 'react';
-import Header from './Header/header.js';
 import Mainbody from './Body/body.js';
 
 function Content() {
     return (
         <div>
-            <Header/>
             <Mainbody/>
         </div>
     )
